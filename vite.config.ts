3d6a794fdeedc6e648db0a9bd54b6b5112a5c435
shehata <msb330033@gmail.com>
2025-07-08 02:06:25 +0300

    Update Vite configuration to change base path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/project-bolt-sb1-ggewxvgw/', // Replace with your repository name
+  base: '/my-portfolio/', // Replace with your repository name
 });
